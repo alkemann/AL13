05928cccef2009bb8a3175782048139b10e0db8f
Nate Abele <nate.abele@gmail.com>
2010-07-07 09:54:09 -0400

    Updating routes for al13_logable to comply with latest API changes.

--- al13_logable/config/routes.php
+++ al13_logable/config/routes.php
@@ -1,19 +1,21 @@
 <?php
-use \lithium\net\http\Router;
+
+use lithium\net\http\Router;
 
 Router::connect('/logs', array(
-	'plugin' => 'al13_logable',	'controller' => 'logs', 'action' => 'index'
+	'library' => 'al13_logable', 'controller' => 'logs', 'action' => 'index'
 ));
 
 Router::connect('/logs/page:{:page:[0-9]+}', array(
-	'plugin' => 'al13_logable',	'controller' => 'logs', 'action' => 'index', 'page' => 1
+	'library' => 'al13_logable', 'controller' => 'logs', 'action' => 'index', 'page' => 1
 ));
 
 Router::connect('/logs/limit:{:limit:[0-9]+}', array(
-	'plugin' => 'al13_logable',	'controller' => 'logs', 'action' => 'index', 'limit' => 10
+	'library' => 'al13_logable', 'controller' => 'logs', 'action' => 'index', 'limit' => 10
 ));
 
 Router::connect('/logs/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}', array(
-	'plugin' => 'al13_logable',	'controller' => 'logs', 'action' => 'index', 'page' => 1, 'limit' => 10
+	'library' => 'al13_logable', 'controller' => 'logs', 'action' => 'index', 'page' => 1, 'limit' => 10
 ));
+
 ?>
