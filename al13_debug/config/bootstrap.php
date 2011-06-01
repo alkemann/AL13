40290ee13571f571d1b113bd150a9371bcb1b16f
alkemann <alek.morland@gmail.com>
2011-06-01 16:12:27 +0200

    moved global functions into the autoloaded bootstrap location and file

--- al13_debug/config/bootstrap.php
+++ al13_debug/config/bootstrap.php
@@ -3,7 +3,11 @@
   * Global convenience methods
   */
 
-// Debug dump any amount of variable
+/**
+ * Dump any amount of paramters in a html styled var_dump
+ * 
+ * @param mixed any amount
+ */
 function d() {
     $debug = \al13_debug\util\Debug::get_instance();
     $args = func_get_args();
@@ -32,11 +36,24 @@ function dd() {
     die('<div style="margin-top: 25px;font-size: 10px;color: #500;">-Debug die-</div>');
 }
 
-// Convenient way of adding / setting a Debug setting
+/**
+ * Convenient way of adding / setting a Debug setting
+ * 
+ * @param string $setting Name/Key of Debug config/setting to set
+ * @param mixed $value Value
+ */
 function ds($setting, $value) {
     \al13_debug\util\Debug::$options[$setting] = $value;
 }
-function dsb($value, $category = 'objects') {
+
+/**
+ * Conventience method for adding to blacklist
+ * If an array it will overwrite the category with the supplied array
+ * 
+ * @param mixed $value Value add to specified blacklist category or array to set the entire category
+ * @param string $category Name of category of blacklist that is being modified, 'property' by default
+ */
+function dsb($value, $category = 'property') {
     if (is_array($value)) {
         \al13_debug\util\Debug::$options['blacklist'][$category] = $value;
         return;
@@ -44,23 +61,42 @@ function dsb($value, $category = 'objects') {
     \al13_debug\util\Debug::$options['blacklist'][$category][] = $value;
 }
 
-// Extra short way of adding a blacklisted array key
+/**
+ * Extra short way of adding a blacklisted array key
+ *
+ * @param mixed $value Name of array key to black list, or array of
+ */
 function dbk($value) {
     $category = 'key';
     dsb($value, $category);
 }
-// Extra short way of adding a blacklisted object property
+
+/**
+ * Extra short way of adding a blacklisted object property
+ * 
+ * @param mixed $value Name of object property to blacklist, or array of
+ */
 function dbp($value) {
     $category = 'property';
     dsb($value, $category);
 }
-// Extra short way of adding a blacklisted class instance
+
+/**
+ * Extra short way of blacklisting classes
+ *
+ * @param mixed $value Name of class, of which objects will be blacklisted, or array of
+ */
 function dbc($value) {
     $category = 'class';
     dsb($value, $category);
 }
 
-// Convenient wrapper for other \util\Debug methods
+/**
+ * Convenient wrapper for other \util\Debug methods
+ * 
+ * @param string $method Name of method to call on the Debug obect
+ * @param boolean $echo True will echo, false will return result
+ */
 function dw($method, $echo = true) {
     $debug = \al13_debug\util\Debug::get_instance();
     $result = $debug->$method();
